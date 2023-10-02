440a11a1238b49ed895db7b76a27df08535a0bcc
John T. Wodder II <git@varonathe.org>
2023-10-02 19:52:00 -0400

    Address a warning

--- rust-test/src/main.rs
+++ rust-test/src/main.rs
@@ -3,7 +3,7 @@ use std::fs::{File, rename};
 
 fn main() -> anyhow::Result<()> {
     let _ = File::create("foo.txt")?;
-    let fp = File::open("Cargo.toml")?;
+    let _fp = File::open("Cargo.toml")?;
     rename("foo.txt", "Cargo.toml").context("failed to move to open file")?;
     Ok(())
 }
