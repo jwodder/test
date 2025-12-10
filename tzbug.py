8f3bcd5c77da6194beb688d6b9a0736431255bdc
John T. Wodder II <git@varonathe.org>
2025-12-10 11:28:29 -0500

    Don't use strftime()

--- tzbug.py
+++ tzbug.py
@@ -2,7 +2,7 @@ import sys
 import time
 
 stamp = 1762623296  # 2025-11-08 12:34:56-05:00
-tzname = time.strftime("%Z", time.localtime(stamp))
+tzname = time.localtime(stamp).tm_zone
 print(f"Got {tzname!r}, expected 'EST'")
 
 if tzname != "EST":
